2e625b5ac241435003751d9ec9b7bf9f586d4edc
Edgars Pelna <regative@gmail.com>
2012-06-02 18:07:09 +0300

    Code-formatting

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -2,9 +2,16 @@
 
 class Model_User extends Model {
     public function check_user_data($nick,$password){
-        return $query = db::select()->from('users')->where('nick', '=', $nick)->and_where('password', '=', $password)->execute();
+        return $query = db::select()
+				->from('users')
+				->where('nick', '=', $nick)
+				->and_where('password', '=', $password)
+				->execute();
     }
     public function get_user_by_session_id($nick){
-        return $query = db::select('name')->from('users')->where('nick', '=', $nick)->execute();
+        return $query = db::select('name')
+				->from('users')
+				->where('nick', '=', $nick)
+				->execute();
     }
 }
