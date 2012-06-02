8cb51c8960f04e05e2fa3355ed0041da6458598f
Edgars Pelna <regative@gmail.com>
2012-06-02 17:55:22 +0300

    Code-formatting

--- application/classes/model/post.php
+++ application/classes/model/post.php
@@ -1,12 +1,12 @@
 <?php
 class Model_Post extends Model {
 	public function insert_post($title,
-	$slug,
-	$category,
-	$date,
-	$author,
-	$introduction,
-	$content){
+			$slug,
+			$category,
+			$date,
+			$author,
+			$introduction,
+			$content){
 		return $query = DB::insert('posts',
 				array('title',
 					'slug',
