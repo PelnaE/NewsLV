24de98ba858a15299ff7f4315f2c3179a8f6eba6
Edgars Pelna <regative@gmail.com>
2012-10-03 15:49:37 +0300

    Start of big revision.

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -4,8 +4,8 @@ class Controller_Home extends Controller_Template {
 
 	public function action_index()
 	{
-		$view = View::factory('home/list');
-		$model_for_posts = Model::factory('post');
+        $view = View::factory('home/list');
+        $model_for_posts = Model::factory('post');
         $get_news = $model_for_posts->get_all_published_posts("jaunumi");
 		$view->news = $get_news;
 		$get_articles = $model_for_posts->get_all_published_posts("raksti");
