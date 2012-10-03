4117dec801ec19d0c263a22e61d3526864548fde
Edgars Pelna <regative@gmail.com>
2012-10-03 15:55:32 +0300

    Adjusting my code to phpcs standards.

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -7,10 +7,11 @@ class Controller_Home extends Controller_Template {
         $view = View::factory('home/list');
         $model_for_posts = Model::factory('post');
         $get_news = $model_for_posts->get_all_published_posts("jaunumi");
-		$view->news = $get_news;
-		$get_articles = $model_for_posts->get_all_published_posts("raksti");
-		$view->articles = $get_articles;
-		$this->template->content = $view->render();
-	}
+        $view->news = $get_news;
+        $get_articles = $model_for_posts->get_all_published_posts("raksti");
+        $view->articles = $get_articles;
+        $this->template->content = $view->render();
+    }
 
 } // End Welcome
+
