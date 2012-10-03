efcb2e7853b58c847ce3797c045f0fd16c3b9b31
Edgars Pelna <regative@gmail.com>
2012-10-03 15:57:57 +0300

    Finished adjusting code of Controller_Home to phpcs standards.

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -4,12 +4,12 @@ class Controller_Home extends Controller_Template {
 
     public function action_index()
     {
-        $view = View::factory('home/list');
-        $model_for_posts = Model::factory('post');
-        $get_news = $model_for_posts->get_all_published_posts("jaunumi");
-        $view->news = $get_news;
-        $get_articles = $model_for_posts->get_all_published_posts("raksti");
-        $view->articles = $get_articles;
+        $view                    = View::factory('home/list');
+        $model_for_posts         = Model::factory('post');
+        $get_news                = $model_for_posts->get_all_published_posts("jaunumi");
+        $view->news              = $get_news;
+        $get_articles            = $model_for_posts->get_all_published_posts("raksti");
+        $view->articles          = $get_articles;
         $this->template->content = $view->render();
     }
 
