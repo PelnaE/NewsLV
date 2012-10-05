e189c73d232bfcb0134089fb839f18b4d2058569
Edgars Pelna <regative@gmail.com>
2012-10-05 16:35:48 +0300

    Model_User adjusted to phpcs standards.

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -1,17 +1,22 @@
 <?php defined('SYSPATH') or die('Hacking attemp!');
 
-class Model_User extends Model {
-    public function check_user_data($nick,$password){
+class Model_User extends Model
+{
+    public function check_user_data($nick,$password)
+    {
         return $query = db::select()
-				->from('users')
-				->where('nick', '=', $nick)
-				->and_where('password', '=', $password)
-				->execute();
+            ->from('users')
+            ->where('nick', '=', $nick)
+            ->and_where('password', '=', $password)
+            ->execute();
     }
-    public function get_user_by_session_id($nick){
+
+    public function get_user_by_session_id($nick)
+    {
         return $query = db::select('name')
-				->from('users')
-				->where('nick', '=', $nick)
-				->execute();
+            ->from('users')
+            ->where('nick', '=', $nick)
+            ->execute();
     }
 }
+
