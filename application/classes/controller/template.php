53c5c123b8d692fa3d44d4986f5a184f9e4f1a8f
Edgars Pelna <regative@gmail.com>
2012-10-05 11:50:46 +0300

    Adjusted Controller_Template to phpcs standards.

--- application/classes/controller/template.php
+++ application/classes/controller/template.php
@@ -1,10 +1,11 @@
-<?php
-defined('SYSPATH') or die('Hacking attemp!');
+<?php defined('SYSPATH') or die('Hacking attemp!');
 
-class Controller_Template extends Kohana_Controller_Template {
-	public $template = 'template/default';
-	public function before(){
-	parent::before();
-	
-	}
+class Controller_Template extends Kohana_Controller_Template
+{
+    public $template = 'template/default';
+    public function before()
+    {
+       parent::before();
+    }
 }
+
