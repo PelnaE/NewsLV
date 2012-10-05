78c8d8670f3460b100c4e57143d72e600e988d9f
Edgars Pelna <regative@gmail.com>
2012-10-05 15:45:53 +0300

    Model_Post adjusted to phpcs standards.

--- application/classes/model/post.php
+++ application/classes/model/post.php
@@ -4,52 +4,55 @@ class Model_Post extends Model
 {
     public function insert_post(array $data)
     {
-		return $query = DB::insert('posts', array_keys($data))
-			->values(array_values($data))
-			->execute();
-	}
+        return $query = DB::insert('posts', array_keys($data))
+        ->values(array_values($data))
+        ->execute();
+    }
 
-	public function get_all_published_posts($category){
-		return $query = DB::select()
-			->from('posts')
-			->where('category', '=', $category)
-			->order_by('id', 'DESC')
-			->execute()
-			->as_array();
-	}
+    public function get_all_published_posts($category)
+    {
+        return $query = DB::select()
+            ->from('posts')
+            ->where('category', '=', $category)
+            ->order_by('id', 'DESC')
+            ->execute()
+            ->as_array();
+    }
 
-	public function get_post_by_id($id){
-		return $query = db::select()
-			->from('posts')
-			->where('id','=',array($id))
-			->as_object()
-			->execute();
-	}
+    public function get_post_by_id($id)
+    {
+        return $query = db::select()
+            ->from('posts')
+            ->where('id','=',array($id))
+            ->as_object()
+            ->execute();
+    }
 
-	public function update_post($title,
-					$content,
-					$introduction,
-					$id){
-		$query = DB::update('posts')
-			->set(array(
-				'title'=>$title,
-				'content'=>$content,
-				'introduction'=>$intro))
-			->where('id','=',$id)
-			->execute();
-	}
+    public function update_post($title, $content, $introduction, $id)
+    {
+        $query = DB::update('posts')
+            ->set(array(
+                'title'=>$title,
+                'content'=>$content,
+                'introduction'=>$intro))
+            ->where('id','=',$id)
+            ->execute();
+    }
 
-	public function get_post_list(){
-		return $query = DB::select()
-			->from('posts')
-			->order_by('id','DESC')
-			->execute()
-			->as_array();
-	}
+    public function get_post_list()
+    {
+        return $query = DB::select()
+            ->from('posts')
+            ->order_by('id','DESC')
+            ->execute()
+            ->as_array();
+    }
 
-	public function delete_post($id){
-		return $query = db::delete('posts')
-			->where('id','=',array($id))
-			->execute();
-	}
+    public function delete_post($id)
+    {
+        return $query = db::delete('posts')
+            ->where('id','=',array($id))
+            ->execute();
+    }
 }
+
